264cb7d5ffca80a46e1c991ad4544a5a46062405
gonzalez.josemartin.994@gmail.com <gonzalez.josemartin.994@gmail.com>
2025-03-18 02:09:34 -0300

    practica listas, maps, outputs

--- practica6/variables.tf
+++ practica6/variables.tf
@@ -1,12 +1,21 @@
 variable "virginia_cidr" {
-    description = "CIDR Virginia"
-    type = string
+  description = "CIDR Virginia"
+  type        = string
 }
-variable "public_subnet" {
-    description = "CIDR public subnet"
-    type = string
+# variable "public_subnet" {
+#     description = "CIDR public subnet"
+#     type = string
+# }
+# variable "private_subnet" {
+#     description = "CIDR private subnet"
+#     type = string
+# }
+
+variable "subnets" {
+  description = "Lista de subnets"
+  type        = list(string)
 }
-variable "private_subnet" {
-    description = "CIDR private subnet"
-    type = string
+variable "tags" {
+  description = "Tags de proyecto"
+  type        = map(string)
 }
--- practica6/vpc.tf
+++ practica6/vpc.tf
@@ -1,17 +1,24 @@
 resource "aws_vpc" "vpc_virginia" {
   cidr_block = var.virginia_cidr
   tags = {
-    Name = "VPC_VIRGINIA"
+    "Name" = "vpc_virginia"
   }
 }
 
 
+
 resource "aws_subnet" "public_subnet" {
-  vpc_id = aws_vpc.vpc_virginia.id
-  cidr_block = var.public_subnet
+  vpc_id                  = aws_vpc.vpc_virginia.id
+  cidr_block              = var.subnets[0]
   map_public_ip_on_launch = true
+  tags = {
+    "Name" = "public_subnet"
+  }
 }
 resource "aws_subnet" "private_subnet" {
-  vpc_id = aws_vpc.vpc_virginia.id
-  cidr_block = var.private_subnet
+  vpc_id     = aws_vpc.vpc_virginia.id
+  cidr_block = var.subnets[1]
+  tags = {
+    "Name" = "private_subnet"
+  }
 }
